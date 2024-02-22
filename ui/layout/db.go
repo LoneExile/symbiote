62d8088ef8b6aa92f8dd60705f63f5569ce77e76
Apinant.U <Apinant-Moo@tutanota.com>
2024-02-22 16:16:33 +0700

    fix(sym): exit instead throwing error

--- ui/layout/db.go
+++ ui/layout/db.go
@@ -1,6 +1,7 @@
 package layout
 
 import (
+	"os"
 	f "symbiote/cmd/aws/fn"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -9,6 +10,9 @@ import (
 func ForwardDB(m model) tea.Cmd {
 	port := m.TextForm.Inputs[0].Value() + ":" + m.TextForm.Inputs[1].Value()
 	c := f.RdsCmd(port, m.CurrentP)
+	if c == nil {
+		os.Exit(1)
+	}
 
 	teaCmds := tea.ExecProcess(c, func(err error) tea.Msg {
 		return commandCompletedMsg{err}
